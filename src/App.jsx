65c7500a4adf9969256397b7747746c664f9680b
Hubert <h.kwiatkowski@hotmail.com>
2023-06-23 17:22:00 +0200

    chore: samples grid UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,46 @@
 import React from "react";
-import Button from 'react-bootstrap/Button';
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import { SoundIcon } from "./icons";
+
 import "./App.css";
 
 const App = () => {
-  return (
-    <>
-      <Button variant="primary">Primary</Button>{' '}
-    </>
-  )
+  const sounds = [
+    "kick",
+    "snare",
+    "hh open",
+    "hh closed",
+    "clap",
+    "rim",
+    "tom",
+  ];
+
+  const tones = () => {
+    return Array.from({ length: 16 }, (_, i) => (
+      <Button variant="light" className="pad"></Button>
+    ));
+  };
+
+  const gridToDisplay = () => {
+    return (
+      <Container className="center-block">
+        {sounds.map((sound, index) => {
+          return (
+            <div className="sound-row" key={index}>
+              <Button variant="light" className="sound-button">
+                {sound}
+                <SoundIcon />
+              </Button>
+              {tones()}
+            </div>
+          );
+        })}
+      </Container>
+    );
+  };
+
+  return <div className="app">{gridToDisplay()}</div>;
 };
 
 export default App;
---
+++ src/icons/SoundIcon.jsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+
+export const SoundIcon = () => (
+  <svg
+    width="18"
+    height="18"
+    viewBox="0 0 18 18"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M9 5.25L9 12.75" stroke="#18121E" strokeLinecap="round" />
+    <path d="M11.25 7.5L11.25 10.5" stroke="#18121E" strokeLinecap="round" />
+    <path d="M13.5 6L13.5 12" stroke="#18121E" strokeLinecap="round" />
+    <path d="M15.75 9.75L15.75 8.25" stroke="#18121E" strokeLinecap="round" />
+    <path d="M6.75 3L6.75 15" stroke="#18121E" strokeLinecap="round" />
+    <path d="M4.5 6.75L4.5 11.25" stroke="#18121E" strokeLinecap="round" />
+    <path d="M2.25 9.75L2.25 8.25" stroke="#18121E" strokeLinecap="round" />
+  </svg>
+);
