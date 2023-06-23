9b2e7578c0575a5693df31040671c31df1fb1300
Hubert <h.kwiatkowski@hotmail.com>
2023-06-23 13:16:24 +0200

    fix: fixed issues in feedback

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import React from "react";
-
+import Button from 'react-bootstrap/Button';
 import "./App.css";
 
 const App = () => {
+  return (
+    <>
+      <Button variant="primary">Primary</Button>{' '}
+    </>
+  )
 };
 
 export default App;
